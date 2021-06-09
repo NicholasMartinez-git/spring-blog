0fc4dccac51d8f3d8e6fcb6bf01f5448a053f1bd
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-06-09 11:21:06 -0500

    end of lecture

---
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -0,0 +1,32 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello from Spring!";
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + "!";
+    }
+    /*
+    * Notice in the above example we also used the @RequestMapping annotation,
+    * which is just the longer version of @GetMapping. There, of course, also exists a
+    * @PostMapping annotation that tells the controller to respond to POST requests.
+    * */
+}
+
