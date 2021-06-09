fc43e6bb62057233b1cde18566efbc91d0f9732d
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-06-09 11:58:58 -0500

    completed Q2 of controller exercises

---
+++ src/main/java/com/codeup/springblog/MathController.java
@@ -0,0 +1,39 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+
+    @GetMapping("/add/{x}/and/{y}")
+    @ResponseBody
+    public int add(@PathVariable int x, @PathVariable int y) {
+        return x + y;
+    }
+
+    @GetMapping("/subtract/{x}/from/{y}")
+    @ResponseBody
+    public int subtract(@PathVariable int x, @PathVariable int y) {
+        return y - x;
+    }
+
+    @GetMapping("/multiply/{x}/and/{y}")
+    @ResponseBody
+    public int multiply(@PathVariable int x, @PathVariable int y) {
+        return x * y;
+    }
+
+    @GetMapping("/divide/{x}/by/{y}")
+    @ResponseBody
+    public String divide(@PathVariable int x, @PathVariable int y) {
+        double sum = (double) x / y;
+        if (x % y == 0) {
+            return String.format("%.0f", sum);
+        } else {
+            return String.format("%.2f", sum);
+        }
+    }
+}
