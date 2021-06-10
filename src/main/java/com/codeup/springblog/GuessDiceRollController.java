9dc50b383446259a482e2d6f8336745e685a80d0
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-06-10 16:59:33 -0500

    Q1 for Views exercises: In Progress (2)

--- src/main/java/com/codeup/springblog/GuessDiceRollController.java
+++ src/main/java/com/codeup/springblog/GuessDiceRollController.java
@@ -1,12 +1,33 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GuessDiceRollController {
+    private int randomDiceRoll = (int) (Math.random() * (6 - 1 + 1)) + 1;
+
     @GetMapping("/roll-dice")
     public String guessDiceRoll() {
+        randomDiceRoll = (int) (Math.random() * (6 - 1 + 1)) + 1;
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String userGuess(@PathVariable int n, Model model) {
+        model.addAttribute("n", n);
+        model.addAttribute("randomNum", randomDiceRoll);
+        String outcome;
+
+        if (n == randomDiceRoll) {
+            outcome = "You won!";
+        } else {
+            outcome = "You lost!";
+        }
+        model.addAttribute("outcome", outcome);
         return "roll-dice";
     }
 }
