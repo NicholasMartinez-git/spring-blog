c9c4106f726bb97dd758dc029c8415698fcf34ef
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-06-11 11:06:42 -0500

    Q2 for Views exercises: completed (2)

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,36 +1,62 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
 
+//    @GetMapping("/posts")
+//    @ResponseBody
+//    public String getPostsIndexPage() {
+//        return "posts index page";
+//    }
+//
+//    @GetMapping("/posts/{id}")
+//    @ResponseBody
+//    public String getPostsById(@PathVariable long id) {
+//        return "view an individual post by id #" + id;
+//    }
+//
+//    @GetMapping("/posts/create")
+//    @ResponseBody
+//    public String createPostsForm() {
+//        return "view form for creating a post";
+//    }
+//
+//    @PostMapping("posts/create")
+//    @ResponseBody
+//    public String createPosts() {
+//        return "create a new post";
+//    }
+
     @GetMapping("/posts")
-    @ResponseBody
-    public String getPostsIndexPage() {
-        return "posts index page";
+    public String getAllPosts(Model model) {
+        List<Post> listOfPosts = new ArrayList<>();
+
+        Post funnyPost = new Post("Funny Post", "Pudding...");
+        Post smartPost = new Post("Smart Post", "Apple + Bananas = Oranges...");
+
+        listOfPosts.add(funnyPost);
+        listOfPosts.add(smartPost);
+
+        model.addAttribute("posts", listOfPosts);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String getPostsById(@PathVariable long id) {
-//        String idNumStr = String.valueOf(id);
-        return "view an individual post by id #" + id;
-    }
-
-    @GetMapping("/posts/create")
-    @ResponseBody
-    public String createPostsForm() {
-        return "view form for creating a post";
-    }
-
-    @PostMapping("posts/create")
-    @ResponseBody
-    public String createPosts() {
-        return "create a new post";
+    public String getOnePost(@PathVariable long id, Model model) {
+        Post post = new Post(id, "My first title", "All the content with in context of my title");
+        model.addAttribute("id", post.getId());
+        model.addAttribute("title", post.getTitle());
+        model.addAttribute("body", post.getBody());
+        return "posts/show";
     }
 }
